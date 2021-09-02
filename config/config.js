20e6f21b64dbf2c85108f543349d11e5dd271342
Darinka Obradovic <obradovicd216@gmail.com>
2021-09-02 21:56:56 +0200

    commit

---
+++ config/config.js
@@ -0,0 +1,12 @@
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
+  mongoUri: "mongodb+srv://projekat123:projekat123@cluster0.evjsw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ||
+    process.env.MONGO_HOST ||
+    'mongodb://' + (process.env.IP || 'localhost') + ':' +
+    (process.env.MONGO_PORT || '27017') +
+    '/mernproject'
+}
+
+export default config
